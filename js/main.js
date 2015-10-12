c3902171b9f40f02ea39b8d6e0b3f9e85eeec6f5
Melissa Wilcox <melissa@emdubb.co>
2015-10-12 12:26:17 -0700

    attempt to store variables for multiple pages

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ console.log("main.js loaded");
 
 $('#beginGame').click(function beginGame(){
 	var $playerName1 = $('#player-name1').val();
+	console.log($playerName1);
 	var $playerName2 = $('#player-name2').val();
 	var $playerName3 = $('#player-name3').val();
 	var $playerName4 = $('#player-name4').val();
 	var $playerName5 = $('#player-name5').val();
 	var $playerName6 = $('#player-name6').val();
+
+	$('#player1').prepend($playerName1);
 });
